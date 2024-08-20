c2075548cecd01f8a3870ecd9eb8cf7314594162
shunich <fukusyun3@gmail.com>
2024-08-20 12:47:53 +0900

    fix

--- src/app/answer/page.tsx
+++ src/app/answer/page.tsx
@@ -3,12 +3,23 @@
 import React, { useState, Suspense } from 'react';
 import styles from './../page.module.css';
 import { useSearchParams } from 'next/navigation';
+import { fetchAnswerText } from '../utils/fetchAnswerText';
+
+const AnswerContent = () => {
+  const searchParams = useSearchParams();
+  const answer = searchParams.get('answer');
+
+  return (
+    <>
+      <h2 className={styles.subTitle}>回答</h2>
+      {answer && <h3>{fetchAnswerText(answer)}</h3>}
+      <h2 className={styles.borderTitle}>{answer}</h2>
+    </>
+  );
+};
 
 const Answer = () => {
   const [isShowText, setIsShowText] = useState(false);
-
-  const searchParams = useSearchParams();
-  const answer = searchParams.get('answer');
 
   const handleShowAnswer = () => {
     setIsShowText(true);
@@ -19,8 +30,7 @@ const Answer = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>終了！ お疲れ様でした！</h1>
         <Suspense fallback={<div>読み込み中...</div>}>
-          {isShowText && <h2 className={styles.subTitle}>回答</h2>}
-          {isShowText && <h2 className={styles.borderTitle}>{answer}</h2>}
+          {isShowText && <AnswerContent />}
         </Suspense>
         {!isShowText && (
           <button
---
+++ src/app/utils/fetchAnswerText.ts
@@ -0,0 +1,22 @@
+export const fetchAnswerText = (answer: string) => {
+  switch (answer) {
+    case 'E1V1M5':
+      return '最初目が閉じている状態: 揺らしても目が開かない';
+    case 'E3V2M6':
+      return '最初目が閉じている状態: 変なことを言う';
+    case 'E3V3M6':
+      return '最初目が閉じている状態: 名前しか言わない';
+    case 'E3V4M6':
+      return '最初目が閉じている状態: しっかり答える';
+    case 'E4V2M6':
+      return '最初目が開いている状態: 変なことを言う';
+    case 'E4V3M6':
+      return '最初目が開いている状態: 名前しか言わない';
+    case 'E4V4M6':
+      return '最初目が開いている状態: しっかり答える';
+    case 'NO_ANSWER':
+      return '手を握らない';
+    default:
+      return '回答が見つかりません';
+  }
+};
