9e46b0a4939b7d0324b01f659d0f7476bb55ac14
shunich <fukusyun3@gmail.com>
2024-08-20 12:37:27 +0900

    fix: bug

--- src/app/answer/page.tsx
+++ src/app/answer/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import styles from './../page.module.css';
 import { useSearchParams } from 'next/navigation';
 
@@ -18,7 +18,10 @@ const Shake = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>終了！ お疲れ様でした！</h1>
-        {isShowText && <h2 className={styles.borderTitle}>{answer}</h2>}
+        <Suspense fallback={<div>読み込み中...</div>}>
+          {isShowText && <h2 className={styles.subTitle}>回答</h2>}
+          {isShowText && <h2 className={styles.borderTitle}>{answer}</h2>}
+        </Suspense>
         {!isShowText && (
           <button
             onClick={handleShowAnswer}
